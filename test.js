73471f319aa39137fdd61dbe8dceac79c1dd730e
wonwo <peacewalker58911@gmail.com>
2021-10-01 17:19:11 +0800

    day5

---
+++ test.js
@@ -0,0 +1,57 @@
+// //透過var宣告變數n，沒給值得話為undefined
+// var n;
+
+// //宣告且給值
+// var m = 1;
+
+// //let與var的差異為變數作用範圍(scope)。
+// let i = 2;
+
+// //const也可以宣告變數。
+// const d = 3;
+
+
+
+// var app = 'Smallapp';
+// var App = 'BigApp';
+// console.log(app,App);
+
+
+
+// var m = 1;
+// console.log(m)
+// var m = 2;
+// console.log(m)
+// var m = 3;
+// console.log(m)
+// let s = 1;
+// console.log(s)
+// let s = 2;
+// console.log(s)
+
+
+// var str = '這不是一行文 \
+// 這是第二行\
+// 這是第三行 ';
+
+// var str = `這不是一行文
+// 這是第二行
+// 這是第三行`;
+
+// var age = 30;
+// var str = `I am a ${age} person`
+
+// console.log(str);
+
+// //內嵌運算式
+// var a =5;
+// var b =10;
+
+// console.log(`Fifteen is ${a + b} and not ${2 * a + b}.`)
+
+// var a = 10;
+// var b = 1.3;
+
+// console.log(typeof(a) == typeof(b))
+
+console.log(isNaN(0.1 + 0.2 === 0.3));
